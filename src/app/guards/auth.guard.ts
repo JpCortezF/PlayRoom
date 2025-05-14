3f1570378427468982576bdd6cf42cd2295588dd
JpCortezF <juanpactz@gmail.com>
2025-05-14 00:20:25 -0300

    Error de redirección cada vez que me iba de la pestaña

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,16 +1,15 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { firstValueFrom } from 'rxjs';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
-  const supabase = auth.sb.supabase;
  
-  const { data: { session } } = await supabase.auth.getSession();
-  
-  if (!session?.user) {
-    // Si no hay usuario, redirigir a login
+  const user = await firstValueFrom(auth.currentUser$);
+
+  if (!user) {
     router.navigate(['/login']);
     return false;
   }
--- src/app/guards/no-auth.guard.ts
+++ src/app/guards/no-auth.guard.ts
@@ -1,17 +1,18 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { firstValueFrom } from 'rxjs';
 
 export const noAuthGuard: CanActivateFn = async (route, state) => {
   const auth = inject(AuthService);
-    const router = inject(Router);
-    const supabase = auth.sb.supabase;
-  
-    const { data: { session } } = await supabase.auth.getSession();
-    
-    if (session?.user && (state.url === '/login' || state.url === '/register')) {
-      router.navigate(['/']);
-      return false;
-    }
-    return true;
+  const router = inject(Router);
+
+  const user = await firstValueFrom(auth.currentUser$);
+
+  if (user) {
+    router.navigate(['/']);
+    return false;
+  }
+
+  return true;
 };
